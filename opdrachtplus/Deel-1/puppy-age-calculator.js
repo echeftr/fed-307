289f7bedd06beef0043bbec348591585e553334b
echeftr <69512919+echeftr@users.noreply.github.com>
2020-10-23 16:48:41 +0200

    Add files via upload

---
+++ opdrachtplus/Deel-1/puppy-age-calculator.js
@@ -0,0 +1,7 @@
+const calculateDogAge = function(age) {
+let dogAge = age * 7;
+console.log('Your Doggie is ' + dogAge + ' years old in dog years!')
+}
+calculateDogAge(2);
+calculateDogAge(6);
+calculateDogAge(10);
