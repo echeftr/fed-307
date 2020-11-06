c3f621ec2ad63232350b4964c9ee33a60d113493
echeftr <69512919+echeftr@users.noreply.github.com>
2020-11-06 15:29:36 +0100

    Delete puppy-age-calculator.js

--- opdrachtplus/Deel-1/puppy-age-calculator.js
+++
@@ -1,7 +0,0 @@
-const calculateDogAge = function(age) {
-let dogAge = age * 7;
-console.log('Your Doggie is ' + dogAge + ' years old in dog years!')
-}
-calculateDogAge(2);
-calculateDogAge(6);
-calculateDogAge(10);
