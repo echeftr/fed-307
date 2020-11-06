d72f499cbcf679e27cabdf11e1ecd1e0dfc5f5d3
echeftr <69512919+echeftr@users.noreply.github.com>
2020-11-06 15:29:18 +0100

    Delete fortune-teller.js

--- opdrachtplus/Deel-1/fortune-teller.js
+++
@@ -1,7 +0,0 @@
-const tellFortune = function(numKids, partnerName, geoLocation, jobTitle) {
-    let future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' + partnerName + ' with ' + numKids + ' kids.';
-    console.log(future);
-};
-tellFortune(3, 'Maria', 'Aruba', 'CEO');
-tellFortune(2, 'Anna', 'Aruba', 'CEO');
-tellFortune(10, 'Cary', 'Aruba', 'CEO');
