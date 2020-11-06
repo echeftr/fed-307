5efd0e3e9e2b4f62e9e94606f5e6b4ab3f421e5c
echeftr <69512919+echeftr@users.noreply.github.com>
2020-11-06 15:28:41 +0100

    Add files via upload

---
+++ Deel2/the-movie-database.js
@@ -0,0 +1,15 @@
+// Niet af
+
+let favMovie = {
+    title: "Puff the magic Dragon",
+    duration: 30,
+    stars: ["Puff", "Jackie", "Living Sneezes"],
+    
+};
+
+let printMovie = function () {
+    console.log(title + " last for " + duration + " minutes. Stars: " + stars
+    )
+}
+
+printMovie(favMovie);
